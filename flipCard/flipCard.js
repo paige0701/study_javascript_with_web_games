bf8e7f835766ecb287dc0fd63e40621804f09c3b
Paige <paigechoi0701@gmail.com>
2020-07-12 20:18:17 +0900

    flip all card at start to give time to memorise

--- flipCard/flipCard.js
+++ flipCard/flipCard.js
@@ -1,7 +1,16 @@
 var 가로 = 4;
 var 세로 = 3;
+var 색깔후보 = ['red', 'red', 'orange','orange', 'green','green', 'yellow', 'yellow','white', 'pink', 'white', 'pink'];
+var 색깔 = [];
+for (var i = 0; 색깔후보.length > 0; i++) {
+    색깔 = 색깔.concat(색깔후보.splice(Math.floor(Math.random() * 색깔후보.length), 1))
+}
+var 클릭플래그 = true;
 
 function 카드세팅(가로, 세로) {
+
+    클릭플래그 = false;
+
     for (var i = 0; i < 가로 * 세로; i++) {
         var card = document.createElement('div');
         card.className = 'card';
@@ -14,6 +23,7 @@ function 카드세팅(가로, 세로) {
 
         var cardBack = document.createElement('div');
         cardBack.className = 'card-back';
+        cardBack.style.backgroundColor = 색깔[i];
 
         cardInner.appendChild(cardFront);
         cardInner.appendChild(cardBack);
@@ -22,12 +32,27 @@ function 카드세팅(가로, 세로) {
         // closure 문제
         (function(c) {
             card.addEventListener('click', () => {
-                c.classList.toggle('flipped');
+                if (클릭플래그) {
+                    c.classList.toggle('flipped');
+                }
             })
         })(card);
 
         document.body.appendChild(card)
     }
+
+    document.querySelectorAll('.card').forEach((card, index) => {
+        setTimeout(() => {
+            card.classList.add('flipped');
+        }, 1000 + 100 * index)
+
+    });
+    setTimeout(() => {
+        document.querySelectorAll('.card').forEach((card) => {
+            card.classList.remove('flipped');
+        });
+        클릭플래그 = true;
+    }, 5000);
 }
 
 카드세팅(가로, 세로);
